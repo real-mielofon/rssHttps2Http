2e891df1ce24fb78c367e47022653c54a550be36
Alexey <mielofon@gmail.com>
2017-07-31 23:04:15 +0300

    parse

--- main.go
+++ main.go
@@ -7,12 +7,18 @@ import (
 )
 
 func main() {
+
 	feed, err := rss.Fetch("http://feeds.rucast.net/Umputun")
 	if err != nil {
 		log.Fatal(err)
 	}
 	
-	fmt.Println(feed)
+	//fmt.Println(feed)
+	for _, item := range feed.Items {
+		for _, en := range item.Enclosures {
+			fmt.Println(en.URL)			
+		}
+	}
 	
 	err = feed.Update()
 	if err != nil {
